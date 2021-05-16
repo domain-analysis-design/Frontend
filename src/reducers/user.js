72da56d0d448bd41775b5b4476948d5b33fbf0ce
jinho <gogod23@ajou.ac.kr>
2021-05-16 16:32:36 +0900

    feat : login featureing

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -7,6 +7,9 @@ export const initialState = {
   userLoading: false,
   userDone: false,
   userError: null,
+  loginLoading: false,
+  loginDone: false,
+  loginError: null,
 };
 
 // action type
@@ -15,9 +18,15 @@ export const LOAD_USER_REQUEST = "user/LOAD_USER_REQUEST";
 export const LOAD_USER_SUCCESS = "user/LOAD_USER_SUCCESS";
 export const LOAD_USER_FAILURE = "user/LOAD_USER_FAILURE";
 
+export const LOGIN_REQUEST = "user/LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "user/LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "user/LOGIN_FAILURE";
+
 // action creator
 
 export const loadUserRequestAction = createAction(LOAD_USER_REQUEST);
+
+export const loginRequestAction = createAction(LOGIN_REQUEST);
 
 // reducer
 
@@ -41,6 +50,25 @@ const user = handleActions(
       userLoading: false,
       userDone: false,
       userError: "error",
+    }),
+    [LOGIN_REQUEST]: (state, action) => ({
+      ...state,
+      loginLoading: true,
+      loginDone: false,
+      loginError: null,
+    }),
+    [LOGIN_SUCCESS]: (state, action) => ({
+      ...state,
+      loginLoading: false,
+      loginDone: true,
+      loginError: null,
+      user: action.user,
+    }),
+    [LOGIN_FAILURE]: (state, action) => ({
+      ...state,
+      loginLoading: false,
+      loginDone: false,
+      loginError: "error",
     }),
   },
   initialState,
