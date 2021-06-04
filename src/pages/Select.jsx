90daae1434e7e19e9a6ae9449d003c53b0f9f682
jin-Pro <dnjun2@naver.com>
2021-06-04 19:33:14 +0900

    chore : redux by jin-Pro

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -4,7 +4,7 @@ import List, { CreateList } from "../components/Select/List";
 import BoardHeader from "../components/Select/BoardHeader";
 import LeftSide from "../components/Select/LeftSide";
 import RightSide from "../components/Select/RightSide";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   loadBoardRequestAction,
   initializeBoardRequestAction,
@@ -26,19 +26,29 @@ const MainBox = styled.div`
 `;
 
 const Select = () => {
-  const BoardInfo = localStorage.getItem("currentBoard");
-  const board = JSON.parse(BoardInfo);
+  //redux에 board가지고오기
+  const {board} = useSelector((state)=>state.board);
+  console.log(board)
+  // const BoardInfo = localStorage.getItem("currentBoard");
+  // const board = JSON.parse(BoardInfo);
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(loadBoardRequestAction());
-    //loadBoardRequestAction : localStorage에 있는 currentBoard 갖고오기
-    return () => {
-      dispatch(initializeBoardRequestAction());
-      // 다 썻으면 초기화 시켜주기
-    };
-  }, [BoardInfo]);
+  // useEffect(() => {
+  //   if (localStorage.getItem("currentBoard")) {
+  //     localStorage.removeItem("currentBoard");
+  //   }
+  //   localStorage.setItem("currentBoard", JSON.stringify(board));
+  // }, [])
+
+  // useEffect(() => {
+  //   // dispatch(loadBoardRequestAction());
+  //   //loadBoardRequestAction : localStorage에 있는 currentBoard 갖고와서  redux저장
+  //   return () => {
+  //     dispatch(initializeBoardRequestAction());
+  //     // 다 썻으면 초기화 시켜주기
+  //   };
+  // }, []);
 
   return (
     <>
